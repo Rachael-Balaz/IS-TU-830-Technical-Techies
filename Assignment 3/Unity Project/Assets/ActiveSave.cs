4966c1e58c56f181de8328276d70769b58538a90
LongNguyen06 <30692436+LongNguyen99@users.noreply.github.com>
2020-10-13 02:19:53 +1100

    Updating new scene + enemy + bunch of animation

---
+++ Assignment 3/Unity Project/Assets/ActiveSave.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveSave : MonoBehaviour
+{
+    public GameObject Trigger;
+
+     void OnTriggerEnter(Collider other)
+    {
+        Trigger.SetActive(true);
+    }
+
+}
---
+++ Assignment 3/Unity Project/Assets/DeactiveSky.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactiveSky : MonoBehaviour
+{
+    public GameObject Trigger;
+    void OnTriggerEnter(Collider other)
+    {
+        Trigger.SetActive(false);
+    }
+}
