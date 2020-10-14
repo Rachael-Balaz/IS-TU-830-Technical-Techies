37678b838e919b3216bca9fbf058744ba47e598a
s3825611 <s3825611@student.rmit.edu.au>
2020-10-14 13:34:31 +1100

    Added scripts + whatever the rest are

---
+++ Assignment 3/Unity Project/Assets/Scripts/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform playerTransform;
+    private Transform transform;
+    private Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = gameObject.GetComponent<Transform>();
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(playerTransform.position.x < -3 || playerTransform.position.x > 3))
+        {
+            pos.x = playerTransform.position.x;
+            transform.position = pos;
+        }
+    }
+}
